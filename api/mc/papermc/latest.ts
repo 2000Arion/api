7820676172593dd130638cb6f6988c557faf0e45
Arion2000 <github@arion2000.xyz>
2023-10-16 16:28:21 +0200

    Update latest.ts

--- api/mc/papermc/latest.ts
+++ api/mc/papermc/latest.ts
@@ -19,40 +19,32 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
         }
         const project_json: any = await route_projects_project.json();
 
-        if (project_json.versions.length > 0) {
-            const latestVersion = project_json[project_json.versions.length - 1];
-            console.log(latestVersion);
+        const latestVersion = project_json[project_json.versions.length - 1];
+        console.log(latestVersion);
 
-            // https://api.papermc.io/v2/projects/${project}/versions/${latestVersion} abfragen und neuesten Build speichern:
-            const route_projects_project_versions_version = await fetch(`https://api.papermc.io/v2/projects/${project}/versions/${latestVersion}`);
-            if (!route_projects_project_versions_version.ok) {
-                throw new Error(`${route_projects_project_versions_version.status} ${route_projects_project_versions_version.statusText}`);
-            }
-            const versions_json: any = await route_projects_project_versions_version.json();
-
-            if (versions_json.builds.length > 0) {
-                const latestBuild = versions_json[versions_json.builds.length - 1];
-                console.log(latestBuild);
-
-                // https://api.papermc.io/v2/projects/${project}/versions/${latestVersion}/builds/${latestBuild} abfragen und neuesten Build speichern:
-                const route_projects_project_versions_version_builds_build = await fetch(`https://api.papermc.io/v2/projects/${project}/versions/${latestVersion}/builds/${latestBuild}`);
-                if (!route_projects_project_versions_version_builds_build.ok) {
-                    throw new Error(`${route_projects_project_versions_version_builds_build.status} ${route_projects_project_versions_version_builds_build.statusText}`);
-                }
-                const build_json: any = await route_projects_project_versions_version_builds_build.json();
-
-                const downloadname = build_json.downloads.application.name;
-                const downloadurl = `https://https://api.papermc.io/v2/projects/${project}/versions/${latestVersion}/builds/${latestBuild}/downloads/${downloadname}`
-
-                // Zu Downloadurl weiterleiten:
-                res.writeHead(302, { Location: downloadurl });
-                res.end();
-            } else {
-                throw new Error(`Das Array unter https://api.papermc.io/v2/projects/${project}/versions/${latestVersion} ist leer.`)
-            }
-        } else {
-            throw new Error(`Das Array unter https://api.papermc.io/v2/projects/${project} ist leer.`)
+        // https://api.papermc.io/v2/projects/${project}/versions/${latestVersion} abfragen und neuesten Build speichern:
+        const route_projects_project_versions_version = await fetch(`https://api.papermc.io/v2/projects/${project}/versions/${latestVersion}`);
+        if (!route_projects_project_versions_version.ok) {
+            throw new Error(`${route_projects_project_versions_version.status} ${route_projects_project_versions_version.statusText}`);
         }
+        const versions_json: any = await route_projects_project_versions_version.json();
+
+        const latestBuild = versions_json[versions_json.builds.length - 1];
+        console.log(latestBuild);
+
+        // https://api.papermc.io/v2/projects/${project}/versions/${latestVersion}/builds/${latestBuild} abfragen und neuesten Build speichern:
+        const route_projects_project_versions_version_builds_build = await fetch(`https://api.papermc.io/v2/projects/${project}/versions/${latestVersion}/builds/${latestBuild}`);
+        if (!route_projects_project_versions_version_builds_build.ok) {
+            throw new Error(`${route_projects_project_versions_version_builds_build.status} ${route_projects_project_versions_version_builds_build.statusText}`);
+        }
+        const build_json: any = await route_projects_project_versions_version_builds_build.json();
+
+        const downloadname = build_json.downloads.application.name;
+        const downloadurl = `https://https://api.papermc.io/v2/projects/${project}/versions/${latestVersion}/builds/${latestBuild}/downloads/${downloadname}`
+
+        // Zu Downloadurl weiterleiten:
+        res.writeHead(302, { Location: downloadurl });
+        res.end();
     } catch (error) {
         console.error(error);
         return res.status(500).json({
