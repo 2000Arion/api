87bab48e77c34c57b168fca168d690fccac420be
Arion2000 <github@arion2000.xyz>
2023-10-16 18:48:50 +0200

    Update

--- api/mc/index.ts
+++ api/mc/index.ts
@@ -4,16 +4,16 @@ import { RedirectToGithub } from '../../static/js/githubref';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
     try {
-        const cssContent = fs.readFileSync('/static/css/index.css', 'utf-8');
+        const cssContent = fs.readFileSync('../../static/css/index.css', 'utf-8');
         res.setHeader('Content-Type', 'text/css');
         res.send(cssContent);
 
         res.send("Weiterleitung...");
 
-        res.send(RedirectToGithub());
+        res.send(RedirectToGithub);
     } catch (error) {
         console.error(error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).send(`Internal Server Error: ${error}`);
     }
     res.end();
 }
