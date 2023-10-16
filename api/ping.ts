f1363fea2cfc2ef4f808599c2f8db0365d36fbe1
Arion2000 <github@arion2000.xyz>
2023-10-16 16:44:53 +0200

    Created ping function

---
+++ api/ping.ts
@@ -0,0 +1,13 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const { lang = 'de' } = req.query;
+  const startTime = process.hrtime();
+  const endTime = process.hrtime(startTime);
+  const durationInMilliseconds = endTime[0] * 1000 + endTime[1] / 1e6;
+  if (lang == 'de' || lang != 'en') {
+    return res.status(200).send(`Die Seite wurde in ${durationInMilliseconds}ms geladen.`)
+  } else if (lang == 'en') {
+    return res.status(200).send(`The page was loaded in ${durationInMilliseconds}ms.`)
+  }
+}
