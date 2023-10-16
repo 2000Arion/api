75bae79c906a3dde261b892519d56b5f9661788d
Arion2000 <github@arion2000.xyz>
2023-10-16 19:08:31 +0200

    Update

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,17 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { RedirectToGithub } from '../static/js/githubref';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
     try {
         res.send("Weiterleitung...");
 
-        res.send(RedirectToGithub);
+        const path = window.location.pathname;
+        if (path.endsWith("index.html")) {
+            let result = path.slice(0, path.length - "index.html".length);
+            return window.location.href = `https://github.com/2000Arion/api/tree/main${result}`;
+        } else {
+            let result = window.location.pathname;
+            return window.location.href = `https://github.com/2000Arion/api/tree/main${result}`;
+        }
     } catch (error) {
         console.error(error);
         res.status(500).send(`${error}`);
