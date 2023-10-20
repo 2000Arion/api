dc7084e69ea866aeab70982cd9d41451bae0171a
Arion2000 <github@arion2000.xyz>
2023-10-20 07:01:07 +0200

    Update ping.ts

--- api/ping.ts
+++ api/ping.ts
@@ -4,11 +4,12 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
   const { lang = 'de' } = req.query;
   const startTime = process.hrtime();
   const endTime = process.hrtime(startTime);
-  const durationInMilliseconds = endTime[0] * 1000 + endTime[1] / 1e6;
+  const duration = endTime[0] * 1000 + endTime[1] / 1e6;
+  const loadingTime = duration * 1000;
   if (lang == 'de' || lang != 'en') {
-    return res.status(200).send(`Die Seite wurde in ${durationInMilliseconds}ms geladen.`)
+    return res.status(200).send(`Die Seite wurde in ${loadingTime}s geladen.`)
   } else if (lang == 'en') {
-    return res.status(200).send(`The page was loaded in ${durationInMilliseconds}ms.`)
+    return res.status(200).send(`The page was loaded in ${loadingTime}s.`)
   }
   res.end();
 }
