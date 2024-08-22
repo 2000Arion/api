2e75a6d8d943e07c09dca5030d5a2e4cd0072bb1
Arion2000 <github@arion2000.xyz>
2024-08-22 14:49:25 +0200

    Update calc_data.ts

--- api/mc/servers/calc_data.ts
+++ api/mc/servers/calc_data.ts
@@ -23,8 +23,8 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
         { id: '${uuid()}', vCPU: 4, RAM: 15, SSD: 150, hourly: 0.0466, monthly: 29.14, fees: 0.21 },
         { id: '${uuid()}', vCPU: 8, RAM: 30, SSD: 230, hourly: 0.0925, monthly: 57.70, fees: 0.24 },
         { id: '${uuid()}', vCPU: 16, RAM: 62, SSD: 350, hourly: 0.1840, monthly: 114.82, fees: 0.26 },
-        { id: '${uuid()}', vCPU: 32, RAM: 125, SSD: 590, hourly: 0.3671, monthly: 229.06, fees: 0.29 },
-        { id: '${uuid()}', vCPU: 48, RAM: 185, SSD: 950, hourly: 0.5501, monthly: 343.30, fees: 0.32 },
+        { id: '${uuid()}', vCPU: 32, RAM: 126, SSD: 590, hourly: 0.3671, monthly: 229.06, fees: 0.29 },
+        { id: '${uuid()}', vCPU: 48, RAM: 186, SSD: 950, hourly: 0.5501, monthly: 343.30, fees: 0.32 },
     ],
 };
     `);
