5ae04708961d70bcf80407d3d3b7ff0aee14c717
Arion2000 <github@arion2000.xyz>
2025-04-05 16:58:08 +0000

    Use cf-connecting-ip header for retrieving ip

--- api/ip.ts
+++ api/ip.ts
@@ -2,11 +2,10 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
-    const response = await fetch("https://api.arion2000.xyz/cdn-cgi/trace");
-    const text = await response.text();
-
-    const ipLine = text.split("\n").find((line) => line.startsWith("ip="))
-    const ip = ipLine ? ipLine.split("=")[1] : "Unknown"
+    const forwarded = req.headers["cf-connecting-ip"] || req.headers["x-forwarded-for"];
+    const ip = Array.isArray(forwarded)
+      ? forwarded[0]
+      : forwarded?.split(",")[0] ?? req.socket.remoteAddress;
 
     res.status(200).send(ip);
     res.end();
