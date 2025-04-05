4ea4914927d873235ec9a0a02ed0f38fa1efbc2a
Arion2000 <aron.sommer09@gmail.com>
2025-04-05 18:50:49 +0200

    Update ip api

--- api/ip.ts
+++ api/ip.ts
@@ -1,9 +1,17 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  try {
+    const response = await fetch("https://api.arion2000.xyz/cdn-cgi/trace");
+    const text = await response.text();
 
-  res.status(200).send(ip);
+    const ipLine = text.split("\n").find((line) => line.startsWith("ip="))
+    const ip = ipLine ? ipLine.split("=")[1] : "Unknown"
 
-  res.end();
+    res.status(200).send(ip);
+    res.end();
+  } catch (error) {
+    console.error("Error retrieving IP adress:", error);
+    res.status(500).send("Error retrieving IP adress");
+  }  
 }
